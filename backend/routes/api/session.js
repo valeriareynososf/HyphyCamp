6f147a08b6cef8ff9134a7bcade5da3b8e5e8a95
valeriareynososf <valeriayearup@gmail.com>
2021-10-07 09:28:30 -0700

    tested get user route

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -28,10 +28,25 @@ router.post(
     });
   }),
 );
+
 router.delete("/", (_req, res) => {
   res.clearCookie("token");
   return res.json({ message: "success" });
 });
+
+// Restore session user
+router.get(
+  '/',
+  restoreUser,
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject()
+      });
+    } else return res.json({});
+  }
+);
 
 module.exports = router;
 /* wcYy963y-tFAddt66mPCJ0xyeyy1seBweFgI
