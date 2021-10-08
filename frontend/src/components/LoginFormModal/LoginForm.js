2a8f74fd85349e0dda5108180f78eb143dcf55e9
valeriareynososf <valeriayearup@gmail.com>
2021-10-07 20:45:51 -0700

    commented out useDispatch and redirect for loginform

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
-import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
+//import { useDispatch, useSelector } from "react-redux";
+//import { Redirect } from "react-router-dom";
 
 import "./LoginForm.css";
 
