734f46ceb511722efc57c9c7532322ff476cd39d
valeriareynososf <valeriayearup@gmail.com>
2021-10-10 11:56:02 -0700

    saving

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,12 +1,13 @@
 // import React, { useState } from "react";
 // import { Modal } from "../../context/Modal";
 // import SignupForm from "./SignUpForm";
+import "./HomePage.css";
 
 function HomePage() {
   //const [showModal, setShowModal] = useState(false);
 
   return (
-    <>
+    <body>
     <h2>HELLO THIS IS THE HOMEPAGE</h2>
       {/* <button onClick={() => setShowModal(true)}>Sign Up</button>
       {showModal && (
@@ -14,7 +15,7 @@ function HomePage() {
           <SignupForm />
         </Modal>
       )} */}
-    </>
+    </body>
   );
 }
 
