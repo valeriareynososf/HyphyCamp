3262e81664067921f061b0b7a47c0659f77ebdd3
valeriareynososf <valeriayearup@gmail.com>
2021-10-17 11:37:57 -0700

    added about modal and page not found

---
+++ frontend/src/components/Navigation/About.js
@@ -0,0 +1,10 @@
+import "./Navigation.css"
+
+function About () {
+    return (
+        <div className="aboutContainer">
+        <p>A Bandcamp clone</p>
+        </div>
+    )
+}
+export default About;
